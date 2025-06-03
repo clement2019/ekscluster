14937f792ee88e3ea650c9fc4032bdb0a7b77f44
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-06-03 09:20:54 +0100

    addedd new files

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket         = "bankapp-terraform-eks-state-s3-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
