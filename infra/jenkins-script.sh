cbe8bc99e83d25a7b02b0b6df627719226ac3461
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-06-09 14:39:03 +0100

    added fresh files

---
+++ infra/jenkins-script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+ 
+sudo apt update -y
+
+sudo apt upgrade -y 
+
+sudo apt install -y docker.io
+
+
+## install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+sudo apt install unzip 
+unzip awscliv2.zip 
+sudo ./aws/install
+
+#check the version
+
+aws --version
+
+echo "Waiting for 15 seconds before installing the eksctl"
+sleep 15
+
+
+echo "Waiting for 29 seconds before installing the aws cli..."
+sleep 15
+
+# install kubectl on the EC2 instance
+
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+kubectl version
+
+echo "Waiting for 30 seconds before installing the aws cli..."
+sleep 15
+
+# Add the current user to the 'docker' group 
+sudo chmod 777 /var/run/docker.sock
+
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Print Docker version
+docker --version
+
+echo "Waiting for 15 seconds before installing the aws cli..."
+sleep 15
+
+sudo apt install openjdk-17-jre -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
+
+
