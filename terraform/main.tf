b3bc475c2392ea98662750221a71b532da9441f5
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-10-09 13:27:26 +0100

    files added1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,101 @@
+resource "aws_iam_role" "eks-iam-role" {
+ name = "devops-eks-iam-role"
 
-provider "aws" {
-  region = var.region
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+
 }
 
-module "vpc" {
-  source = "./modules/vpc"
-
-  vpc_cidr             = var.vpc_cidr
-  availability_zones   = var.availability_zones
-  private_subnet_cidrs = var.private_subnet_cidrs
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  cluster_name         = var.cluster_name
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
 }
 
-module "eks" {
-  source = "./modules/eks"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnet_ids
-  node_groups     = var.node_groups
+resource "aws_eks_cluster" "my-eks" {
+ name = var.cluster_name
+ role_arn = aws_iam_role.eks-iam-role.arn
+
+ vpc_config {
+
+
+  subnet_ids = [var.public_subnet1,var.public_subnet2]
+ }
+
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
 }
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.my-eks.name
+  node_group_name = "my-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids = [var.public_subnet1,var.public_subnet2]
+  instance_types = ["t2.medium"]
+
+  scaling_config {
+   desired_size = 2
+   max_size   = 2
+   min_size   = 1
+  }
+
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+ }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,65 +1,18 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-west-2"
+
+# networking
+
+
+variable "public_subnet1" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "subnet-98dba5e2"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+variable "public_subnet2" {
+  description = "CIDR Block for PublicSubnet 1"
+  default     = "subnet-4356da0f"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "my-eks-cluster"
-}
-
-variable "cluster_version" {
-  description = "Kubernetes version"
-  type        = string
-  default     = "1.30"
-}
-
-variable "node_groups" {
-  description = "EKS node group configuration"
-  type = map(object({
-    instance_types = list(string)
-    capacity_type  = string
-    scaling_config = object({
-      desired_size = number
-      max_size     = number
-      min_size     = number
-    })
-  }))
-  default = {
-    general = {
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-      scaling_config = {
-        desired_size = 2
-        max_size     = 4
-        min_size     = 1
-      }
-    }
-  }
-}
+  type = string
+  default = "my-eks-cluster1"
+ }
